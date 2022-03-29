fe84e577a494bccdd19ca63e1b0291a6eb683516
hiisma <isma.ao.inf@gmail.com>
2022-03-29 16:06:03 +0200

    Afegit constructor i atributs

--- src/main/java/com/mastersdeluniverso/Emoji.java
+++ src/main/java/com/mastersdeluniverso/Emoji.java
@@ -1,5 +1,17 @@
 package com.mastersdeluniverso;
 
+
+import java.util.Vector;
+
+import acm.graphics.GImage;
+
+
 public class Emoji {
-    
+    // Atributs
+    private GImage normal;
+    private GImage zombificado;
+
+    // Constructores
+    Emoji(String dir_img_normal, String dir_img_zombie) {
+    }
 }
