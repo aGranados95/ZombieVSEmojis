5c86323c02cbd9acbf8a9afb2cba6dab6af95e56
hiisma <isma.ao.inf@gmail.com>
2022-03-31 15:13:29 +0200

    Update rapid

--- src/main/java/com/mastersdeluniverso/App.java
+++ src/main/java/com/mastersdeluniverso/App.java
@@ -3,7 +3,6 @@ package com.mastersdeluniverso;
 public class App {
 
     public static void main(String[] args) {
-        new Grafics().start();
         System.out.println("Hello World!");
 
     }
