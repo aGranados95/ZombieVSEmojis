4280fd216565ea20a0f20b075217b2f080872f06
hiisma <isma.ao.inf@gmail.com>
2022-04-01 12:36:32 +0200

    Update rapid

--- src/main/java/com/mastersdeluniverso/Emoji.java
+++ src/main/java/com/mastersdeluniverso/Emoji.java
@@ -18,7 +18,7 @@ public class Emoji {
      * Velocitat de moviment l'emoji. Pot variar
      * durant el joc.
      */
-    private Vector2d velocitat = new Vector2d(0,0);
+    private Vector2d velocitat = new Vector2d(0, 0);
 
     /**
      * Imatges de l'emoji.
@@ -27,7 +27,7 @@ public class Emoji {
     private GImage zombificado;
 
     /**
-     * Indica si l'emoji està zombificat o no. 
+     * Indica si l'emoji està zombificat o no.
      */
     private boolean esZombie;
 
@@ -42,22 +42,65 @@ public class Emoji {
     // Constructores
     /**
      * Constructor amb paràmetres.
+     * 
      * @param dir_img_normal directori de la imatge del emoji
-     * normal.
+     *                       normal.
      * @param dir_img_zombie directori de la imatge del emoji
-     * zombificat.
-     * @param v posició del emoji.
+     *                       zombificat.
+     * @param v              posició del emoji.
      */
     Emoji(String dir_img_normal, String dir_img_zombie, Vector2d pos) {
         normal = new GImage(dir_img_normal);
         zombificado = new GImage(dir_img_zombie);
-        esZombie = false;   // Per defecte l'emoji no està zombificat.
+        esZombie = false; // Per defecte l'emoji no està zombificat.
         this.pos = new Vector2d(pos);
     }
 
     // Mètodes
+    /**
+     * Realitza el moviment de l'emoji.
+     * @param deltaTime
+     */
     public void moures(Double deltaTime) {
         Vector2d nouMoviment = new Vector2d(velocitat.multiplicarNatural(deltaTime));
-        
+
+    }
+
+    // Getters
+
+    /**
+     * Retorna la imatge de l'emoji, segons l'estat actual
+     * 
+     * @return imatge normal si esZombie es fals
+     *         o imatge zombificat si esZombie es cert.
+     */
+    public GImage getImatge() {
+        if (esZombie) {
+            return zombificado;
+        } else {
+            return normal;
+        }
+    }
+
+    /**
+     * @return la posició de l'emoji com Vector2d.
+     */
+    public Vector2d getPos() {
+        return pos;
+    }
+
+    /**
+     * @return si l'emoji es zombie.
+     */
+    public boolean esZombie() {
+        return esZombie;
+    }
+
+    // Setters
+    /**
+     * Transforma l'emoji en zombie.
+     */
+    public void setZombificat() {
+        esZombie = true;
     }
 }
